f91f9284044a0eba574889e48d2fb9e5a9670be9
arsilva87 <anderson.silva@ifgoiano.edu.br>
2022-03-08 13:15:29 +0100

    fix clipPaper

--- R/clipPaper.R
+++ R/clipPaper.R
@@ -1,6 +1,6 @@
 #' Clip Image to Extract the Water-Sensitive Paper
 #'
-#' Click on three points on the image to clip the water-sensitive paper, 
+#' Click on three points on the image to clip the water-sensitive paper,
 #' for further analysis.
 #'
 #' @param x Either an object of class \code{Image} from package [EBImage]
@@ -23,14 +23,17 @@
 clipPaper <- function(x)
 {
    if(inherits(x, "Image")) im <- x else im <- readImage(x)
+   X11()
    display(im, method = "raster")
    getGraphicsEvent("Click on 3 points to clip the image...",
       onMouseDown = function(buttons, x, y) NA)
    loc <- locator(type = "o", col = "red", lwd = 2, n = 3L)
    xint <- min(loc$x):max(loc$x)
    yint <- min(loc$y):max(loc$y)
-   polygon(x = c(min(xint), max(xint), max(xint), min(xint)), 
-      y = c(min(yint), min(yint), max(yint), max(yint)), 
+   polygon(x = c(min(xint), max(xint), max(xint), min(xint)),
+      y = c(min(yint), min(yint), max(yint), max(yint)),
       col = adjustcolor("red", alpha = 0.5))
+   Sys.sleep(3)
+   dev.off()
    return(im[xint, yint, ])
 }
